c54bc657e8a9732e2f3c5ea7d3c0b6784ca81bae
daniel chaves <danielchaves001@gmail.com>
2020-11-12 19:36:49 -0300

    remoção da biblioteca bootstrap4

--- principal/models.py
+++ principal/models.py
@@ -1,68 +1,70 @@
 from django.contrib.auth import get_user_model
-from django.db import models
+from django.db.models import Model
+from django.db.models import CharField
+from django.db.models import ForeignKey
+from django.db.models import ManyToManyField
+from django.db.models import EmailField
+from django.db.models import BooleanField
+from django.db.models import PositiveIntegerField
+from django.db.models import CASCADE
+from django.db.models import SET_NULL
 
-class UpperCharField(models.CharField):
-    def __init__(self, *args, **kwargs):
-        super(UpperCharField, self).__init__(*args, **kwargs)
 
-    def get_prep_value(self, value):
-        return str(value).upper()
-
-class Tipo(models.Model):
-    tipo = UpperCharField(unique=True,max_length=100)
+class Tipo(Model):
+    tipo = CharField(unique=True, max_length=100)
 
     def __str__(self):
         return self.tipo
 
 
-class Palavras(models.Model):
-    palavra = UpperCharField(
+class Palavras(Model):
+    palavra = CharField(
         unique=True,
         max_length=100,
         null=False,
         blank=False
     )
-    tipos = models.ManyToManyField(Tipo)
+    tipos = ManyToManyField(Tipo)
 
     def __str__(self):
         return self.palavra
 
 
-class Jogador(models.Model):
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    nome = UpperCharField(unique=True, max_length=100)
-    email = models.EmailField(unique=True)
+class Jogador(Model):
+    user = ForeignKey(get_user_model(), on_delete=CASCADE)
+    nome = CharField(unique=True, max_length=100)
+    email = EmailField(unique=True)
 
     def __str__(self):
         return self.nome
 
 
-class Jogo(models.Model):
-    jogo = UpperCharField(unique=True, max_length=6, blank=False, null=False)
-    ativo = models.BooleanField(default=False)
-    tipo = models.ForeignKey(Tipo, on_delete=models.SET_DEFAULT, blank=True, null=True,default=None)
-    letra = UpperCharField(max_length=1, blank=True,)
-    ultima_letra = UpperCharField(max_length=1, blank=True)
-    palavra = UpperCharField(max_length=100, blank=True)
-    admin = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    preferencias = models.ManyToManyField(Tipo, related_name='preferencias')
+class Jogo(Model):
+    jogo = CharField(unique=True, max_length=6, blank=False, null=False)
+    ativo = BooleanField(default=False)
+    tipo = ForeignKey(Tipo, on_delete=SET_NULL, blank=True, null=True, default=None)
+    letra = CharField(max_length=1, blank=True, )
+    ultima_letra = CharField(max_length=1, blank=True)
+    palavra = CharField(max_length=100, blank=True)
+    admin = ForeignKey(get_user_model(), on_delete=CASCADE)
+    preferencias = ManyToManyField(Tipo, related_name='preferencias')
 
     def __str__(self):
         return self.jogo
 
 
-class Jogos(models.Model):
-    jogo = models.ForeignKey(Jogo, on_delete=models.CASCADE)
-    jogador = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    ordem = models.IntegerField()
+class Jogos(Model):
+    jogo = ForeignKey(Jogo, on_delete=CASCADE)
+    jogador = ForeignKey(get_user_model(), on_delete=CASCADE)
+    ordem = PositiveIntegerField()
 
     def __str__(self):
         return self.jogador
 
 
-class PalavrasUsadas(models.Model):
-    jogo = models.ForeignKey(Jogo, on_delete=models.CASCADE)
-    palavra = models.ForeignKey(Palavras, on_delete=models.CASCADE)
+class PalavrasUsadas(Model):
+    jogo = ForeignKey(Jogo, on_delete=CASCADE)
+    palavra = ForeignKey(Palavras, on_delete=CASCADE)
 
     def __str__(self):
         return self.palavra
